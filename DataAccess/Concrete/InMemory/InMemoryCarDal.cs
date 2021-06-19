43816d2fd3b0545ca305beaaa5c9b791df658461
Görkem Akan <gorkemakan123@gmail.com>
2021-06-19 17:20:10 +0300

    Day 7

--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -19,7 +19,7 @@ namespace DataAccess.Concrete.InMemory
                 new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2021,DailyPrice=1000,Description="BMW 730d"}, //Siyah BMW 730d
                 new Car{Id=2,BrandId=1,ColorId=2,ModelYear=1992,DailyPrice=500,Description="BMW 520i"}, //Beyaz BMW 520i
                 new Car{Id=3,BrandId=2,ColorId=1,ModelYear=1998,DailyPrice=300,Description="Volkswagen Golf Mk4"}, //Siyah VW Golf Mk4 
-                new Car{Id=4,BrandId=2,ColorId=3,ModelYear=1998,DailyPrice=700,Description="Volkswagen Golf Mk8"}, //Mavi VW Golg Mk8
+                new Car{Id=4,BrandId=2,ColorId=3,ModelYear=1998,DailyPrice=700,Description="Volkswagen Golf Mk8"}, //Mavi VW Golf Mk8
                 new Car{Id=5,BrandId=3,ColorId=2,ModelYear=2016,DailyPrice=450,Description="Fiat Linea 1.3 Multijet"}, //Beyaz
                 
             };
